e71b1f17e01ba907ecbeb0f212bc67ca14a4ab94
Jongho Jeon <hojong.jjh@gmail.com>
2021-07-14 02:15:37 +0900

    Add leetcode 27

---
+++ leetcode/easy/p27.py
@@ -0,0 +1,15 @@
+"""
+27. Remove Element
+https://leetcode.com/problems/remove-element/
+"""
+from typing import List
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
